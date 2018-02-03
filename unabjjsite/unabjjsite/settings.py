04c2b511bcf7b0d8afe846188d17af976971253f
izzie1349 <izzie1349@gmail.com>
2018-02-02 19:59:16 -0500

    deployments

--- unabjjsite/unabjjsite/settings.py
+++ unabjjsite/unabjjsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8-%dvs%&h^0#b1bv=v7v=+roan*a)834$zjch%l5&+guzclxid'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unabjj2.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -60,7 +60,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
@@ -134,3 +134,67 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
+
+
+
+
+# random logging settings, change later:
+
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'filters': {
+    'require_debug_false': {
+        '()': 'django.utils.log.RequireDebugFalse',
+    },
+    'require_debug_true': {
+        '()': 'django.utils.log.RequireDebugTrue',
+    },
+},
+'formatters': {
+    'django.server': {
+        '()': 'django.utils.log.ServerFormatter',
+        'format': '[%(server_time)s] %(message)s',
+    }
+},
+'handlers': {
+    'console': {
+        'level': 'INFO',
+        'filters': ['require_debug_true'],
+        'class': 'logging.StreamHandler',
+    },
+    # Custom handler which we will use with logger 'django'.
+    # We want errors/warnings to be logged when DEBUG=False
+    'console_on_not_debug': {
+        'level': 'WARNING',
+        'filters': ['require_debug_false'],
+        'class': 'logging.StreamHandler',
+    },
+    'django.server': {
+        'level': 'INFO',
+        'class': 'logging.StreamHandler',
+        'formatter': 'django.server',
+    },
+    'mail_admins': {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'django.utils.log.AdminEmailHandler'
+    }
+},
+'loggers': {
+    'django': {
+        'handlers': ['console', 'mail_admins', 'console_on_not_debug'],
+        'level': 'INFO',
+    },
+    'django.server': {
+        'handlers': ['django.server'],
+        'level': 'INFO',
+        'propagate': False,
+    },
+}
+}
