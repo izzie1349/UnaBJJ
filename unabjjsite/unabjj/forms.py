b8b6106d185770a03250ea5eb60a69131d13f1de
Israel Alvarez <ialvarez@nyc01alee.local>
2018-02-26 17:02:18 -0500

    form: add phone number

--- unabjjsite/unabjj/forms.py
+++ unabjjsite/unabjj/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
+    contact_phone = forms.CharField(required=False)
     content = forms.CharField(widget=forms.Textarea, required=False)
